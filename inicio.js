cf6e4d987bcbe640a69ddf488c5dcbfe4b22acd6
Fabian <lfotalora08@gmail.com>
2023-11-25 14:16:27 -0500

    Adaptado a movil

--- inicio.js
+++ inicio.js
@@ -6,11 +6,26 @@ import { btnScrollSp } from "./js/scrollSpye.js"
 
 const d = document
 
+
+function btnBur(btn,panel, anclas){
+    const $panel = d.querySelector(panel)
+
+    d.addEventListener("click", e=> {
+        if(e.target.matches(btn)){
+            $panel.classList.toggle("is-active")
+        }
+        if(e.target.matches(anclas)){
+            $panel.classList.remove("is-active")
+        }
+    })
+}
+
 d.addEventListener("DOMContentLoaded", e=> {
     btnScrollSp()
     fases(".nav-fases a", ".fases")
     scroB(".btn-scroll")
     carruSlide(".slider-hijo", ".anterior",".siguiente")
+    btnBur(".header-btn",".panel",".menu a")
 })
 
 lsBtn(".btn-dark",".todo", "h2","h3","li","p", "a","[data-capsula]")
